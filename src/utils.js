c30dc33c0edf6fb7975d6ccb13bc62c67a1cbe2d
andre <andremindurimg@gmail.com>
2023-04-18 19:49:40 -0300

    Remove utils.js

--- src/utils.js
+++
@@ -1,7 +0,0 @@
-function getArray(buffer, byteOffset, length) {
-  const F64Arr = new Float64Array(buffer, byteOffset, length),
-    Arr = Array.from(F64Arr);
-  return Arr;
-}
-
-module.exports = { getArray };
