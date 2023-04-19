d4ac7edd3371c2d6b798497650d7ec8a6151074d
andre <andremindurimg@gmail.com>
2023-04-19 17:50:19 -0300

    Update statistics.ts

--- src/statistics.ts
+++ src/statistics.ts
@@ -1,12 +1,28 @@
+/**
+  Calculates the mean value of an array of numbers
+  @param values - An array of numbers
+  @throws - If the input array is empty
+  @returns - The mean value of the array
+*/
+
 export function mean(values: number[]): number {
-  if (values.length == 0) throw new Error("Insert an array with at least one number.");
+  if (values.length == 0)
+    throw new Error("Insert an array with at least one number.");
   const SUM = values.reduce((acc, val) => acc + val, 0),
     RESULT = SUM / values.length;
   return RESULT;
 }
 
+/**
+ Calculates the geometric mean of an array of numbers
+ @param values - An array of numbers
+ @throws - If the input array is empty or if it contains both positive and negative values
+ @returns - The geometric mean of the array
+ */
+
 export function geometricMean(values: number[]): number {
-  if (values.length == 0) throw new Error("Insert an array with at least one number.");
+  if (values.length == 0)
+    throw new Error("Insert an array with at least one number.");
   const PRODUCT = values.reduce((acc, val) => acc * Math.abs(val), 1),
     NO_NEGATIVE_VALUES = values.every((a) => a >= 0),
     NO_POSITIVE_VALUES = values.every((a) => a <= 0);
@@ -14,13 +30,21 @@ export function geometricMean(values: number[]): number {
     throw new Error("The values must be either all positive or all negative.");
   }
   const RESULT = NO_NEGATIVE_VALUES
-    ? Math.pow(PRODUCT, 1 / values.length)
-    : -Math.pow(PRODUCT, 1 / values.length);
+    ?  PRODUCT ** (1 / values.length)
+    : - (PRODUCT ** (1 / values.length));
   return RESULT;
 }
 
+/**
+ Calculates the harmonic mean of an array of numbers
+ @param values - An array of numbers
+ @throws - If the input array is empty or if it contains zero values
+ @returns - The harmonic mean of the array
+ */
+
 export function harmonicMean(values: number[]): number {
-  if (values.length == 0) throw new Error("Insert an array with at least one number.");
+  if (values.length == 0)
+    throw new Error("Insert an array with at least one number.");
   const SOME_ZEROS = values.includes(0);
   if (SOME_ZEROS) throw new Error("All values must be greater than zero.");
   const SUM = values.reduce((acc, val) => acc + 1 / val, 0),
@@ -28,8 +52,16 @@ export function harmonicMean(values: number[]): number {
   return RESULT;
 }
 
+/**
+  Calculates the median value of an array of numbers
+  @param values - An array of numbers
+  @throws - If the input array is empty
+  @returns - The median value of the array
+*/
+
 export function median(values: number[]): number {
-  if (values.length == 0) throw new Error("Insert an array with at least one number.");
+  if (values.length == 0)
+    throw new Error("Insert an array with at least one number.");
   const SortedValues = values.sort((a, b) => a - b);
   if (SortedValues.length % 2) {
     return SortedValues[Math.floor(SortedValues.length / 2)];
@@ -42,8 +74,16 @@ export function median(values: number[]): number {
   }
 }
 
+/**
+  Calculates the mode(s) of an array of numbers
+  @param values - An array of numbers
+  @throws - If the input array is empty
+  @returns - An array of the mode(s) of the array
+*/
+
 export function mode(values: number[]): number[] {
-  if (values.length == 0) throw new Error("Insert an array with at least one number.");
+  if (values.length == 0)
+    throw new Error("Insert an array with at least one number.");
   const Frequencies: Map<number, number> = new Map(
     Array.from(new Set(values)).map((num) => [num, 0])
   );
@@ -61,18 +101,38 @@ export function mode(values: number[]): number[] {
   return RESULT;
 }
 
+/**
+  Calculates the range of a given array of numbers.
+  @param values - An array of numbers.
+  @throws - Error if the array is empty.
+  @return - The range of the given array.
+*/
+
 export function range(values: number[]): number {
-  if (values.length == 0) throw new Error("Insert an array with at least one number.");
+  if (values.length == 0)
+    throw new Error("Insert an array with at least one number.");
   const MIN_VALUE = Math.min(...values),
     MAX_VALUE = Math.max(...values),
     RESULT = MAX_VALUE - MIN_VALUE;
   return RESULT;
 }
 
+/**
+  Calculates the midrange of a given array of numbers.
+  @param values - An array of numbers.
+  @return - The midrange of the given array.
+*/
+
 export function midrange(values: number[]): number {
   const RESULT = range(values) / 2;
   return RESULT;
 }
+
+/**
+  Calculates the variance of a given array of numbers.
+  @param values - An array of numbers.
+  @return - The variance of the given array.
+*/
 
 export function variance(values: number[]): number {
   const MU = mean(values),
@@ -81,10 +141,22 @@ export function variance(values: number[]): number {
   return RESULT;
 }
 
+/**
+  Calculates the standard deviation of a given array of numbers.
+  @param values - An array of numbers.
+  @return - The standard deviation of the given array.
+*/
+
 export function standardDeviation(values: number[]): number {
   const RESULT = Math.sqrt(variance(values));
   return RESULT;
 }
+
+/**
+  Calculates the sample variance of a given array of numbers.
+  @param values - An array of numbers.
+  @return - The sample variance of the given array.
+*/
 
 export function sampleVariance(values: number[]): number {
   const MU = mean(values),
@@ -92,6 +164,12 @@ export function sampleVariance(values: number[]): number {
     RESULT = SUM / (values.length - 1);
   return RESULT;
 }
+
+/**
+  Calculates the sample standard deviation of a given array of numbers.
+  @param values - An array of numbers.
+  @return - The sample standard deviation of the given array.
+*/
 
 export function sampleStandardDeviation(values: number[]): number {
   const RESULT = Math.sqrt(sampleVariance(values));
