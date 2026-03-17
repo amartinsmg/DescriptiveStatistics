d3a9616a1573a8587c119cdabf5ff48a467cc6e0
André Martins <andremindurimg@gmail.com>
2026-03-17 01:13:00 -0300

    refactor: Improve data processing and display logic
    - Introduce `getStr` for consistent number formatting
    - Simplify output assignment with new helper function
    - Refactor variable names for clarity and consistency

--- src/main.ts
+++ src/main.ts
@@ -13,107 +13,87 @@ import {
   sampleStandardDeviation,
 } from "./ts/statistics";
 
+function getStr(num: number): string {
+  if (isFinite(num) && !isNaN(num)) return num.toString();
+  else return "-";
+}
+
 // This is the main function that handles the form submit event to perform statistical calculations on the set of numerical data based on user input.
 
 function main() {
-  const Form = document.querySelector("#input-form") as HTMLFormElement,
-    NumbersInput = document.querySelector(
-      "#numbers-input"
+  const form = document.querySelector("#input-form") as HTMLFormElement,
+    datasetInput = document.querySelector(
+      "#dataset-input",
     ) as HTMLTextAreaElement,
-    SampleRadio = document.querySelector(
-      "#sample-pop-sample"
+    sampleRatio = document.querySelector(
+      "#sample-pop-sample",
     ) as HTMLInputElement,
-    ErrFeedbackDiv = document.querySelector("#err-feedback") as HTMLElement,
-    OutEl = document.querySelector("#out") as HTMLElement,
-    DataSetDiv = document.querySelector("#data-set") as HTMLElement,
-    CoutDiv = document.querySelector("#cout") as HTMLElement,
-    ArithmeticMeanDiv = document.querySelector(
-      "#arithmetic-mean"
+    errFeedbackDiv = document.querySelector("#err-feedback") as HTMLElement,
+    outElement = document.querySelector("#out") as HTMLElement,
+    sortedDtsDiv = document.querySelector("#sorted-dataset") as HTMLElement,
+    coutDiv = document.querySelector("#cout") as HTMLElement,
+    minDiv = document.querySelector("#min") as HTMLElement,
+    maxDiv = document.querySelector("#max") as HTMLElement,
+    rangeDiv = document.querySelector("#range") as HTMLElement,
+    midrangeDiv = document.querySelector("#midrange") as HTMLElement,
+    arithmeticMeanDiv = document.querySelector(
+      "#arithmetic-mean",
     ) as HTMLElement,
-    HarmonicMeanDiv = document.querySelector("#harmonic-mean") as HTMLElement,
-    GeometricMeanDiv = document.querySelector("#geometric-mean") as HTMLElement,
-    MedianDiv = document.querySelector("#median") as HTMLElement,
-    ModeDiv = document.querySelector("#mode") as HTMLElement,
-    MinDiv = document.querySelector("#min") as HTMLElement,
-    MaxDiv = document.querySelector("#max") as HTMLElement,
-    RangeDiv = document.querySelector("#range") as HTMLElement,
-    MidrangeDiv = document.querySelector("#midrange") as HTMLElement,
-    ArithmeticVarianceDiv = document.querySelector("#variance") as HTMLElement,
-    ArithmeticSDDiv = document.querySelector(
-      "#standard-deviation"
-    ) as HTMLElement;
+    medianDiv = document.querySelector("#median") as HTMLElement,
+    modeDiv = document.querySelector("#mode") as HTMLElement,
+    arithmeticVarianceDiv = document.querySelector("#variance") as HTMLElement,
+    arithmeticStdDevDiv = document.querySelector(
+      "#standard-deviation",
+    ) as HTMLElement,
+    harmonicMeanDiv = document.querySelector("#harmonic-mean") as HTMLElement,
+    geometricMeanDiv = document.querySelector("#geometric-mean") as HTMLElement;
 
   /**
-    @brief Heandles the form submit event.
-    @param e - The event object.
+   * Heandles the form submit event.
+   * @param e - The event object.
   */
-  Form.addEventListener("submit", (e) => {
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
     try {
-      const { isInteger } = Number,
-        StrNumbers = NumbersInput.value.split(/\s*,\s*/),
-        Numbers = StrNumbers.map((s) => parseFloat(s)).filter((n) => !isNaN(n)),
-        SAMPLE = SampleRadio.checked,
-        NO_ZEROS = !Numbers.includes(0),
-        NO_NEGATIVE_VALUES = Numbers.every((a) => a >= 0),
-        SortedArr = Numbers.sort((a, b) => a - b),
-        ARITHMETIC_MEAN = mean(Numbers),
-        GEOMETRIC_MEAN = NO_NEGATIVE_VALUES ? geometricMean(Numbers) : NaN,
-        HARMONIC_MEAN =
-          NO_ZEROS && NO_NEGATIVE_VALUES ? harmonicMean(Numbers) : NaN,
-        MEDIAN = median(Numbers),
-        Mode = mode(Numbers),
-        MIN = Math.min(...Numbers),
-        MAX = Math.max(...Numbers),
-        RANGE = range(Numbers),
-        MIDRANGE = midrange(Numbers),
-        VARIANCE = SAMPLE ? sampleVariance(Numbers) : variance(Numbers),
-        STANDARD_DEVIATION = SAMPLE
-          ? sampleStandardDeviation(Numbers)
-          : standardDeviation(Numbers);
-      OutEl.classList.remove("d-none");
-      ErrFeedbackDiv.textContent = "";
-      DataSetDiv.textContent = SortedArr.join(", ");
-      CoutDiv.textContent = SortedArr.length.toString();
-      ArithmeticMeanDiv.textContent = isFinite(ARITHMETIC_MEAN)
-        ? isInteger(ARITHMETIC_MEAN * 1e6)
-          ? ARITHMETIC_MEAN.toString()
-          : ARITHMETIC_MEAN.toFixed(6)
-        : "-";
-      GeometricMeanDiv.textContent =
-        isFinite(GEOMETRIC_MEAN) && !isNaN(GEOMETRIC_MEAN)
-          ? isInteger(GEOMETRIC_MEAN * 1e6)
-            ? GEOMETRIC_MEAN.toString()
-            : GEOMETRIC_MEAN.toFixed(6)
-          : "-";
-      HarmonicMeanDiv.textContent = !isNaN(HARMONIC_MEAN)
-        ? isInteger(HARMONIC_MEAN * 1e6)
-          ? HARMONIC_MEAN.toString()
-          : HARMONIC_MEAN.toFixed(6)
-        : "-";
-      MedianDiv.textContent = MEDIAN.toString();
-      ModeDiv.textContent = Mode.join(", ");
-      MinDiv.textContent = MIN.toString();
-      MaxDiv.textContent = MAX.toString();
-      RangeDiv.textContent = isInteger(RANGE * 1e6)
-        ? RANGE.toString()
-        : RANGE.toFixed(6);
-      MidrangeDiv.textContent = isInteger(MIDRANGE * 1e6)
-        ? MIDRANGE.toString()
-        : MIDRANGE.toFixed(6);
-      ArithmeticVarianceDiv.textContent = isFinite(VARIANCE)
-        ? isInteger(VARIANCE * 1e6)
-          ? VARIANCE.toString()
-          : VARIANCE.toFixed(6)
-        : "-";
-      ArithmeticSDDiv.textContent = isFinite(STANDARD_DEVIATION)
-        ? isInteger(STANDARD_DEVIATION * 1e6)
-          ? STANDARD_DEVIATION.toString()
-          : STANDARD_DEVIATION.toFixed(6)
-        : "-";
+      const strDataset = datasetInput.value.split(/\s*(,|;)\s*/),
+        numDataset = strDataset
+          .map((s) => parseFloat(s))
+          .filter((n) => !isNaN(n)),
+        sample = sampleRatio.checked,
+        SortedArr = numDataset.sort((a, b) => a - b),
+        arithmeticMean = mean(numDataset),
+        geomMean = geometricMean(numDataset),
+        harmMean = harmonicMean(numDataset),
+        medianVal = median(numDataset),
+        modeArr = mode(numDataset),
+        min = Math.min(...numDataset),
+        max = Math.max(...numDataset),
+        rangeVal = range(numDataset),
+        midrangeVal = midrange(numDataset),
+        varianceVal = sample
+          ? sampleVariance(numDataset)
+          : variance(numDataset),
+        stdDev = sample
+          ? sampleStandardDeviation(numDataset)
+          : standardDeviation(numDataset);
+      outElement.classList.remove("d-none");
+      errFeedbackDiv.textContent = "";
+      sortedDtsDiv.textContent = SortedArr.join(", ");
+      coutDiv.textContent = SortedArr.length.toString();
+      minDiv.textContent = getStr(min);
+      maxDiv.textContent = getStr(max);
+      rangeDiv.textContent = getStr(rangeVal);
+      midrangeDiv.textContent = getStr(midrangeVal);
+      arithmeticMeanDiv.textContent = getStr(arithmeticMean);
+      medianDiv.textContent = getStr(medianVal);
+      modeDiv.textContent = modeArr.length ? modeArr.join(", ") : "-";
+      arithmeticStdDevDiv.textContent = getStr(stdDev);
+      arithmeticVarianceDiv.textContent = getStr(varianceVal);
+      geometricMeanDiv.textContent = getStr(geomMean);
+      harmonicMeanDiv.textContent = getStr(harmMean);
     } catch (err) {
-      OutEl.classList.add("d-none");
-      ErrFeedbackDiv.textContent =
+      outElement.classList.add("d-none");
+      errFeedbackDiv.textContent =
         err instanceof Error ? err.message : String(err);
     }
   });
