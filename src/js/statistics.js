74ab5f9b54340c84531a8c498b5692c6f6de0345
amartinsmg <andremindurimg@gmail.com>
2026-08-10 19:28:01 -0300

    refactor(js): refactor statistics.ts as statistics.js

--- src/js/statistics.js
+++ src/js/statistics.js
@@ -4,7 +4,7 @@
  * @returns The mean value of the array
 */
 
-export function mean(arr: number[]): number {
+export function mean(arr) {
   if (!arr.length) return NaN;
   const sum = arr.reduce((acc, n) => acc + n, 0),
     result = sum / arr.length;
@@ -17,7 +17,7 @@ export function mean(arr: number[]): number {
  * @returns The geometric mean of the array
  */
 
-export function geometricMean(arr: number[]): number {
+export function geometricMean(arr) {
   if (!arr.length || !arr.every((n) => n >= 0)) return NaN;
   const somaLog = arr.reduce((acc, n) => acc + Math.log(n), 0),
     gMeanLog = somaLog / arr.length,
@@ -31,7 +31,7 @@ export function geometricMean(arr: number[]): number {
  * @returns The harmonic mean of the array
 */
 
-export function harmonicMean(arr: number[]): number {
+export function harmonicMean(arr) {
   if (!arr.length || !arr.every((n) => n > 0)) return NaN;
   const sum = arr.reduce((acc, n) => acc + 1 / n, 0),
     result = arr.length / sum;
@@ -44,7 +44,7 @@ export function harmonicMean(arr: number[]): number {
  * @returns The median value of the array
 */
 
-export function median(arr: number[]): number {
+export function median(arr) {
   if (!arr.length) return NaN;
   const sortedArr = arr.sort((a, b) => a - b),
     mid = Math.floor(arr.length / 2),
@@ -59,10 +59,10 @@ export function median(arr: number[]): number {
  * @returns An array of the mode(s) of the array
 */
 
-export function mode(arr: number[]): number[] {
+export function mode(arr) {
   if (!arr.length) return [];
-  const Frequencies = new Map<number, number>(
-      Array.from(new Set<number>(arr)).map((n) => [
+  const Frequencies = new Map(
+      Array.from(new Set(arr)).map((n) => [
         n,
         arr.filter((x) => x === n).length,
       ]),
@@ -88,7 +88,7 @@ export function mode(arr: number[]): number[] {
  * @return The range of the given array.
 */
 
-export function range(arr: number[]): number {
+export function range(arr) {
   const result = Math.max(...arr) - Math.min(...arr);
   return result;
 }
@@ -99,7 +99,7 @@ export function range(arr: number[]): number {
  * @return The midrange of the given array.
 */
 
-export function midrange(arr: number[]): number {
+export function midrange(arr) {
   const result = range(arr) / 2;
   return result;
 }
@@ -110,7 +110,7 @@ export function midrange(arr: number[]): number {
  * @return The variance of the given array.
 */
 
-export function variance(arr: number[]): number {
+export function variance(arr) {
   const meanVal = mean(arr),
     squaredDeviations = arr.map((n) => (n - meanVal) ** 2),
     result = mean(squaredDeviations);
@@ -123,7 +123,7 @@ export function variance(arr: number[]): number {
  * @return The standard deviation of the given array.
 */
 
-export function standardDeviation(arr: number[]): number {
+export function standardDeviation(arr) {
   const result = Math.sqrt(variance(arr));
   return result;
 }
@@ -134,7 +134,7 @@ export function standardDeviation(arr: number[]): number {
  * @return The sample variance of the given array.
 */
 
-export function sampleVariance(arr: number[]): number {
+export function sampleVariance(arr) {
   if (arr.length <= 1) return NaN;
   const meanVal = mean(arr),
     squaredDeviations = arr.map((n) => (n - meanVal) ** 2),
@@ -149,7 +149,7 @@ export function sampleVariance(arr: number[]): number {
  * @return The sample standard deviation of the given array.
 */
 
-export function sampleStandardDeviation(arr: number[]): number {
+export function sampleStandardDeviation(arr) {
   const result = Math.sqrt(sampleVariance(arr));
   return result;
 }
